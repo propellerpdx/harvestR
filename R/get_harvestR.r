5d85006789dfda227f3f2d192637058ef0c2e071
Christopher Fry <Chris@ChristophersAir.attlocal.net>
2019-01-18 11:08:31 -0800

    Changes to force the use of HTTP/1.1 to account for idiosyncracies between default HTTP protocol choices by different curl versions. Harvest API only supports HTTP/1.1.
    + Formatting for readability

--- R/get_harvestR.r
+++ R/get_harvestR.r
@@ -37,21 +37,38 @@ get_table <- function(
   if(!is.null(query$return_df)){
     return_df <- query[[length(query)]]
     query <- query[[1:(length(query)-1)]]}
+
   if(is.null(query$page)){query$page <- '1'}
+
   url <- paste0('v2/',table) %>% httr::modify_url(url="https://api.harvestapp.com",path=.,query=query)
+
+  # The curl version installed on EC2 / Lambda is slightly different from local.
+  # Harvest's API responds that it can support HTTP/2 when it doesn't seem to be able to
+  # Due to curl version or possibly some other factor, local curl figures this out and gets by it, using HTTP/1.1 while the one on EC2 does not and uses HTTP/2, resulting in failure.
+  # http_version=2 forces HTTP/1.1
+  # Full http_version options can be seen here https://github.com/curl/curl/blob/master/include/curl/curl.h by searching for "http_version"
+  # Full httr docs https://cran.r-project.org/web/packages/httr/httr.pdf
+
+  # Below line will provide verbose output (helpful for troubleshooting)
+  # response = httr::with_config(config=config(verbose=TRUE,http_version=2), httr::GET(url,httr::add_headers("Harvest-Account-ID" = user,Authorization = key,'User-Agent=Propeller R API Helper (mdruffel@propellerpdx.com)', 'From' = email), query = query))
   response_df <- url %>%
-    purrr::map(., function(x) httr::GET(x,httr::add_headers("Harvest-Account-ID" = user,Authorization = key,'User-Agent=Propeller R API Helper (mdruffel@propellerpdx.com)', 'From' = email), query = query)) %>%
+    purrr::map(., function(x) httr::with_config(config=config(http_version=2), httr::GET(x,httr::add_headers("Harvest-Account-ID" = user,Authorization = key,'User-Agent=Propeller R API Helper (mdruffel@propellerpdx.com)', 'From' = email), query = query))) %>%
     purrr::map(., function(x) httr::content(x, as="text", encoding = "UTF-8")) %>%
     purrr::map(., function(x) jsonlite::fromJSON(x, flatten = T))
+
   next_page <- response_df %>% purrr::map('next_page')
+
   if(!exists('return_df')){return_df <- NULL}
   return_df <- response_df %>%
     purrr::map(.,paste0(table)) %>%
     purrr::flatten_df(.) %>%
     dplyr::bind_rows(.,return_df)
+
   query$page <- paste0(unlist(next_page))
   if(is.null(unlist(next_page))){return(return_df)}else{
     query <- list(query=query,return_df=return_df)
+
     if(response_df %>% purrr::map_dbl(.,'total_pages')>99){naptime::naptime(.1)}
+
     get_table(table=table,user=user,key=key,email=email,query=query)}
 }
