5d2d7c12c3dde73eb0363ea09492cd113f5b94a4
Chris Fry <cfry@propellerpdx.com>
2019-05-23 12:07:03 -0700

    Updates to allow a user to store Harvest API credentials in system env variables.

--- R/authentication.R
+++ R/authentication.R
@@ -1,29 +1,58 @@
-#' Retrieve Harvest Personal Access Token
+#' @title Retrieve Harvest Personal Access Token
 #'
-#' Looks in env var HARVEST_PAT
+#' @description A convenience function to allow you to store your Harvest
+#'   API key in a system env variable
+#'
+#' @details The HARVEST_PAT env variable should contain your bearer token,
+#'   formatted as "Bearer [token goes here]". You can get an API key by
+#'   registering at \href{https://id.getharvest.com/developers}{Harvest
+#'   Developers}.
+#'
+#'   NOTE: The user is advised to do this as her/his own risk and this method
+#'   may not be wise for production systems. System env variables are *not* a
+#'   secure storage mechanism. They are in plain text and a hacker could
+#'   theoretically gain access to your API key once they have access to your
+#'   machine. The recommended approach is to store your API key in a secure
+#'   location, like your keychain, and pull it from there to pass to the
+#'   harvestR package.
 #'
 #' @return A string of the Harvest Personal Access Token
+#'
 #' @export
 harvest_pat <- function() {
   var <- Sys.getenv('HARVEST_PAT')
   if (identical(var, "")) {
-    warning("Please set env var HARVEST_PAT to your Harvest personal access token",
+    stop("Please pass an API key or set the env var HARVEST_PAT",
          call. = FALSE)
     return(NULL)
   }
   var
 }
 
-#' Retrieve Harvest Account ID
+#' @title Retrieve Harvest Account ID
 #'
-#' Looks in env var HARVEST_ACCOUNT_ID
+#' @description A convenience function to allow you to store your Harvest
+#'   account ID in a system env variable
+#'
+#' @details The HARVEST_ACCOUNT_ID env variable should contain your Harvest
+#'   account ID. Register at \href{https://id.getharvest.com/developers}{Harvest
+#'   Developers}.
+#'
+#'   NOTE: The user is advised to do this as her/his own risk and this method
+#'   may not be wise for production systems. System env variables are *not* a
+#'   secure storage mechanism. They are in plain text and a hacker could
+#'   theoretically gain access to your API key once they have access to your
+#'   machine. The recommended approach is to store your API key in a secure
+#'   location, like your keychain, and pull it from there to pass to the
+#'   harvestR package.
 #'
 #' @return A string of the Harvest Account ID
+#'
 #' @export
 harvest_id <- function() {
   var <- Sys.getenv('HARVEST_ACCOUNT_ID')
   if (identical(var, "")) {
-    warning("Please set env var HARVEST_ACCOUNT_ID to your Harvest account ID",
+    stop("Please pass a Harvest account ID or set the env var HARVEST_ACCOUNT_ID",
          call. = FALSE)
     return(var)
   }
--- R/get_harvestR.r
+++ R/get_harvestR.r
@@ -1,38 +1,61 @@
-#' Harvest API v2 get request wrapper
+#' @title Harvest API v2 get request wrapper
 #'
-#' Submits get requests to the Harvest API v2. The function is a wrapper around the httr::GET function and returns the Harvest table.
+#' @description Submits get requests to the Harvest API v2. The function is a
+#'   wrapper around the httr::GET function and returns the Harvest table.
 #'
-#' @param table Harvest API table that does not require specific ID's for the GET request. Refer to \href{https://help.getharvest.com/api-v2}{Harvest APIv2}. Possible values include the following as a string:  (\code{"clients"} or \code{"invoices"} or \code{"estimates"} or \code{"expenses"} or \code{"tasks"} or \code{"time_entries"} or \code{"projects"} or \code{"roles"} or \code{"users"})
-#' @param user Harvest account number for authentication. Register at \href{https://id.getharvest.com/developers}{Harvest Developers}.
-#' @param key Harvest API key for API authentication. Register at \href{https://id.getharvest.com/developers}{Harvest Developers}.
-#' @param email Optional argument, requester email address for the "From" header in the get request.
-#' @param query Optional argument, API query parameters to be provided in a list. Refer to \href{https://help.getharvest.com/api-v2}{Harvest APIv2} for acceptable parameters for each table. A few examples include:  (\code{from = "2018-1-1"} or \code{to = "2018-3-31"} or \code{project_id = "1234"} or \code{client_id = "1234"} or \code{user_id = "1234"})
-#' @param verbose logical; passed to httr; A verbose connection provides much more information about the flow of information between the client and server. \href{https://github.com/r-lib/httr}{httr documentation}
+#' @param table Harvest API table that does not require specific ID's for the
+#'   GET request. Refer to \href{https://help.getharvest.com/api-v2}{Harvest
+#'   APIv2}. Possible values include the following as a string:
+#'   (\code{"clients"} or \code{"invoices"} or \code{"estimates"} or
+#'   \code{"expenses"} or \code{"tasks"} or \code{"time_entries"} or
+#'   \code{"projects"} or \code{"roles"} or \code{"users"})
+#' @param user Harvest account number for authentication. Register at
+#'   \href{https://id.getharvest.com/developers}{Harvest Developers}.
+#' @param key Harvest API key for authentication. Register at
+#'   \href{https://id.getharvest.com/developers}{Harvest Developers}.
+#' @param email Optional argument, requester email address for the "From" header
+#'   in the get request.
+#' @param query Optional argument, API query parameters to be provided in a
+#'   list. Refer to \href{https://help.getharvest.com/api-v2}{Harvest APIv2} for
+#'   acceptable parameters for each table. A few examples include:  (\code{from
+#'   = "2018-1-1"} or \code{to = "2018-3-31"} or \code{project_id = "1234"} or
+#'   \code{client_id = "1234"} or \code{user_id = "1234"})
+#' @param verbose logical; passed to httr; A verbose connection provides much
+#'   more information about the flow of information between the client and
+#'   server. \href{https://github.com/r-lib/httr}{httr documentation}
 #'
 #' @return tbl_df
 #'
 #' @examples
-#'
+#'\dontrun{
 #' api_key <- 'your_user_id'
-#' get_table(table = 'projects', user = user_id,key = api_key)
-#' get_table(table = 'time_entries' , user = user_id, key = api_key,
-#'  email = 'your_email',query = list(from = '2018-01-01', to = '2018-03-31'))
+#' get_table(table = 'projects',
+#'           user = user_id,
+#'           key = api_key)
+#' get_table(table = 'time_entries',
+#'           user = user_id,
+#'           key = api_key,
+#'           email = 'your_email',
+#'           query = list(from = '2018-01-01',
+#'                        to = '2018-03-31')
+#'           )
+#'}
 #'
 #' @author Mark Druffel, \email{mdruffel@propellerpdx.com}
-#' @references \url{https://id.getharvest.com/developers}, \url{https://help.getharvest.com/api-v2}, \url{https://github.com/r-lib/httr}
+#' @references \url{https://id.getharvest.com/developers},
+#'   \url{https://help.getharvest.com/api-v2},
+#'   \url{https://github.com/r-lib/httr}
 #'
 #' @importFrom magrittr %>%
 #' @import httr
 #'
 #' @export
-get_table <- function(
-  table = NULL,
-  user = harvest_id(),
-  key = harvest_pat(),
-  email = '',
-  query=NULL,
-  verbose=FALSE)
-{
+get_table <- function(table = NULL,
+                      user = harvest_id(),
+                      key = harvest_pat(),
+                      email = '',
+                      query = NULL,
+                      verbose = FALSE) {
 
   if(!is.null(key) && !grepl("^Bearer ", key))
     key <- paste0('Bearer ', key)
@@ -45,18 +68,36 @@ get_table <- function(
 
   url <- paste0('v2/',table) %>% httr::modify_url(url="https://api.harvestapp.com",path=.,query=query)
 
-  # The curl version installed on EC2 / Lambda is slightly different from local.
-  # Harvest's API responds that it can support HTTP/2 when it doesn't seem to be able to
-  # Due to curl version or possibly some other factor, local curl figures this out and gets by it, using HTTP/1.1 while the one on EC2 does not and uses HTTP/2, resulting in failure.
+  # Harvest's API responds that it can support HTTP/2, but it doesn't seem to
+  # actually be able to. Some versions of curl use HTTP/2 and fail as a
+  # consequence while others figure it out and get by it, using HTTP/1.1 (e.g.
+  # the curl version installed on AWS EC2 / Lambda suffers from this problem)
   # http_version=2 forces HTTP/1.1
-  # Full http_version options can be seen here https://github.com/curl/curl/blob/master/include/curl/curl.h by searching for "http_version"
+  # Full http_version options can be seen here
+  # https://github.com/curl/curl/blob/master/include/curl/curl.h by searching
+  # for "http_version"
   # Full httr docs https://cran.r-project.org/web/packages/httr/httr.pdf
 
   response_df <- url %>%
-    purrr::map(., function(x) httr::with_config(config=config(verbose=verbose, http_version=2), httr::GET(x,httr::add_headers("Harvest-Account-ID" = user,Authorization = key,'User-Agent=Propeller R API Helper (mdruffel@propellerpdx.com)', 'From' = email), query = query))) %>%
+    purrr::map(.,
+               function(x)
+                 httr::with_config(
+                   config = config(verbose = verbose,
+                                   http_version = 2),
+                   httr::GET(
+                     x,
+                     httr::add_headers(
+                       "Harvest-Account-ID" = user,
+                       Authorization = key,
+                       'User-Agent=Propeller R API Helper (mdruffel@propellerpdx.com)',
+                       'From' = email
+                     ),
+                     query = query
+                   )
+                 )) %>%
     purrr::map(., function(x) httr::content(x, as="text", encoding = "UTF-8")) %>%
     purrr::map(., function(x) jsonlite::fromJSON(x, flatten = T))
-
+  print("made it!")
   next_page <- response_df %>% purrr::map('next_page')
 
   if(!exists('return_df')){return_df <- NULL}
